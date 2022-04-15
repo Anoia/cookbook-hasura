365e4fe06821ba23fec62591f6c42b031b254d36
Anoia <github@stuckinadrawer.com>
2022-04-15 10:59:38 +0200

    Add index column to recipe_ingredients

---
+++ migrations/default/1650003587605_alter_table_public_recipe_ingredients_add_column_index/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."recipe_ingredients" add column "index" integer
+--  not null default '0';
